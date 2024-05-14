01520c3b3c22447866331169203de236c5ac73d3
Charlotte Fouques <charlottefouques10@gmail.com>
2024-05-14 15:50:28 +0200

    layout

---
+++ frontend/src/components/layout-elements/LayoutClient.tsx
@@ -0,0 +1,10 @@
+function LayoutClient ({ children }: { children: JSX.Element }){
+
+    return (
+        <div>
+            <h1>test</h1>
+        </div>
+    )
+}
+
+export default LayoutClient; 
