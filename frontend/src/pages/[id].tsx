fac66d8541720eca00276e353c5f0d33d965c7de
Charlotte Fouques <charlottefouques10@gmail.com>
2024-06-13 17:26:36 +0200

    station

--- frontend/src/pages/[id].tsx
+++ frontend/src/pages/[id].tsx
@@ -10,7 +10,6 @@ function MaterialDetail() {
   // const { id } = router.query;
   const [getAd, { data, loading, error }] = useLazyQuery(GET_MATERIAL_BY_ID);
   const { addToCart } = useCart();
-  console.log("material",data)
 
   useEffect(() => {
     if (router.query.id) {
@@ -40,19 +39,26 @@ function MaterialDetail() {
 
   return (
     <main className="container mx-auto px-4 py-8 font-poppins">
-      <h1 className="text-3xl text-neutral-950 font-bold mb-8">
-        {material?.name}
-      </h1>
-      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-        <div className="relative h-48">
-          <img src={material?.picture} alt={material?.name} />
+      
+      <div className="bg-white flex rounded-lg shadow-lg overflow-hidden">
+
+        <div className="flex-auto w-64 ">
+            <img 
+              src={material?.picture}
+              alt={material?.name}
+            />
         </div>
+
         <div className="p-6 flex-auto w-32">
           <h1 className="text-3xl text-neutral-950 font-bold mb-8">
         {material?.name}
       </h1>
           <p className="text-gray ">{material?.description}</p>
           <p className="text-gray w-40">{material?.price}€</p>
+          <p className="text-gray w-40">{material?.size}</p>
+
+        <div className="p-6">
+
           <div className="mt-4 flex justify-end">
             <button onClick={handleAddToCart} className="px-4 py-2 bg-neutral-950 text-white rounded hover:bg-neutral-100 hover:text-neutral-950 hover:font-bold cursor-pointer">
               Ajouter au panier
