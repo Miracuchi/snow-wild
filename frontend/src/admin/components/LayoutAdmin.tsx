4d0629f678a1ae42b5f00b2c3b50d6e44e2591e1
Laurent Vuillaume <laurent.vuillaume@weblib.eu>
2024-06-16 11:41:46 +0200

    add admin part products

--- frontend/src/admin/components/LayoutAdmin.tsx
+++ frontend/src/admin/components/LayoutAdmin.tsx
@@ -1,6 +1,7 @@
 import SideBar from "./Sidebar"
 import HeaderAdmin from "./HeaderAdmin"
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster"
+
 function LayoutAdmin ({ children }: { children: JSX.Element }) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
@@ -10,7 +11,6 @@ function LayoutAdmin ({ children }: { children: JSX.Element }) {
         
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-slate-100">
           {children}
-          <Toaster />
           <Toaster />
         </main>
       </div>
