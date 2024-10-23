489b6d32ba320b3cd92ccef8131326c3a5fe4293
Dylan Dorvilma <dda971@live.fr>
2024-10-23 16:23:49 +0200

    fix: added port listen number 5432

--- backend/src/db.prod.ts
+++ backend/src/db.prod.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm'
 export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT), // Assure-toi de convertir en nombre
+  port: 5432, // Assure-toi de convertir en nombre
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
