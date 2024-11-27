0fc211b669e6d31b6508e870b88c499fc7680166
Dylan Dorvilma <dda971@live.fr>
2024-11-27 23:05:22 +0100

    fix: change import case

--- frontend/src/user/components/layout-elements/Topbar.tsx
+++ frontend/src/user/components/layout-elements/Topbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import DropdownUser from "../dropdown/Dropdownuser";
+import DropdownUser from "../dropdown/DropdownUser";
 import CartIcon from "./CartIcon";
 import LogoIcon from "./LogoIcon";
 import MenuItems from "./MenuItems";
